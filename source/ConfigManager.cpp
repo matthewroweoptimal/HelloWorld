b95083b0a53840b100e15b6e542348f3f299ee33
iquarmby1 <96119171+iquarmby1@users.noreply.github.com>
2023-01-13 17:16:34 +0000

    AudioBackMeterThreadIn
    Still not metering correctly.
    Fix for audio resets users EQ, will need sorting, without this the DSP goes into BIST and audio stops. Suspect garbage being sent to DSP...

--- source/ConfigManager.cpp
+++ source/ConfigManager.cpp
@@ -2109,6 +2109,21 @@ namespace oly {
     			olyStatus.Values[i] = gOLYspeaker1StatusParameterTable[i].defValue;
     		}
     
+    		/* TODO IQ - these are an add from restore defaults. Without them the DSP seems to enter BIST. Odd, I assume it is sent garbage, this will need fixing.*/
+		#if USER_PRESETS_STORED
+    		/* Reset Editable User parameters */
+    		for (i=0; i< USER_PRESETS_STORED; i++) {
+				RestoreUserEQ(i);
+    		}
+
+		#if USE_CDD_UI
+    		//until it is written to, user 1 should not be loaded
+    		//Store 0xFFFFFFFF in the null first word to mark as unuseable
+    		olyStoredParams.Stored_User_Presets[eCDD_PRESETS_USER][0] = 0xFFFFFFFF;
+		#endif
+
+		#endif
+
         	// send a sync current state message to DSP
         	olyDspPort.WriteMessage(GetSyncMessage(MANDOLIN_SE_SYNC_CURRENT_STATE));
 
