e3d26fa75aefaffb38356eaec02f2af6a5b9f6a8
StuartUk1 <stuart.uk.1@gmail.com>
2023-04-27 14:08:06 +0100

    Still a problem with Release builds (with no optimisation). No visible crash on serial output, but not responsive on network. Have built with debug optimisation at the moment as these don't crash. Images added under 'IMAGES/V1.1.16' directory.

--- CDDLive/copyimages.bat
+++ CDDLive/copyimages.bat
@@ -6,14 +6,16 @@ if "%1"=="HELP" goto HELP
 if "%1"=="/?" goto HELP
 
 if "%1"=="" set IMAGES_DIR=.\IMAGES
+if "%1"=="" set DEBUG_DIR=.\IMAGES\Debug
 if not "%1"=="" set IMAGES_DIR=.\IMAGES\%1
-
-if not exist %IMAGES_DIR% md %IMAGES_DIR%
+if not "%1"=="" set DEBUG_DIR=.\IMAGES\%1\Debug
 
 echo ============================================================
 echo Running copyimages to populate %IMAGES_DIR% directory
 echo ============================================================
 
+if not exist %IMAGES_DIR% md %IMAGES_DIR%
+del %IMAGES_DIR%\* /F /Q
 xcopy "Release\OLY_APPLICATION*.img"       %IMAGES_DIR% /y /i
 xcopy "Release 8\OLY_APPLICATION*.img"     %IMAGES_DIR% /y /i
 xcopy "Release 12\OLY_APPLICATION*.img"    %IMAGES_DIR% /y /i
@@ -23,6 +25,17 @@ xcopy "Release 218\OLY_APPLICATION*.img"   %IMAGES_DIR% /y /i
 xcopy "Release Manf\OLY_APPLICATION*.img"  %IMAGES_DIR% /y /i
 
 xcopy "..\BOOTLOADER\BOOTLOADER.bin"  %IMAGES_DIR% /y /i
+
+REM Copying of Debug Images commented out for now. 
+REM if not exist %DEBUG_DIR% md %DEBUG_DIR%
+REM del %DEBUG_DIR%\* /F /Q
+REM xcopy "Debug\OLY_APPLICATION*.img"       %DEBUG_DIR% /y /i
+REM xcopy "Debug 8\OLY_APPLICATION*.img"     %DEBUG_DIR% /y /i
+REM xcopy "Debug 12\OLY_APPLICATION*.img"    %DEBUG_DIR% /y /i
+REM xcopy "Debug 15\OLY_APPLICATION*.img"    %DEBUG_DIR% /y /i
+REM xcopy "Debug 118\OLY_APPLICATION*.img"   %DEBUG_DIR% /y /i
+REM xcopy "Debug 218\OLY_APPLICATION*.img"   %DEBUG_DIR% /y /i
+REM xcopy "Debug Manf\OLY_APPLICATION*.img"  %DEBUG_DIR% /y /i
 
 
 echo Done, %IMAGES_DIR% directory now populated.
